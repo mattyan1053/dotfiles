f79240d19baeb7c3865ee1ad27208acc6a4b46ff
mattyan1053 <h.matsumaruka14161@gmail.com>
2026-02-08 06:24:11 +0900

    :recycle: Fix nvim path for linux

--- run_once_01_nvim_packer_sync.sh
+++ run_once_01_nvim_packer_sync.sh
@@ -2,7 +2,7 @@
 # nvimのプラグインを同期する
 if command -v nvim >/dev/null 2>&1; then
   echo "Running PackerSync..."
-  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' 2>/dev/null
 else
   echo "nvim not found, skipping PackerSync."
 fi
